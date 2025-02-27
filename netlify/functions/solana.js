d74c239bebeee8f24960f22d46c1da119d4d6c5b
kunjpatelx <kunjpatelx@gmail.com>
2025-02-27 16:53:03 +0530

    Update solana.js

--- netlify/functions/solana.js
+++ netlify/functions/solana.js
@@ -44,6 +44,7 @@ exports.handler = async function (event) {
 
         return {
             statusCode: 200,
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify(coinData),
         };
     } catch (error) {
